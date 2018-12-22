c667274c10481f4df9b9baab2f587c71a603082c
Bingo Bobo <matmazur90@gmail.com>
2018-12-22 15:05:59 +0100

    initial commit / spring boot initialized

---
+++ src/main/java/com/matmazur/timeregistratordemo/TimeRegistratorDemoApplication.java
@@ -0,0 +1,14 @@
+package com.matmazur.timeregistratordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeRegistratorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeRegistratorDemoApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/matmazur/timeregistratordemo/TimeRegistratorDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.matmazur.timeregistratordemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TimeRegistratorDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
