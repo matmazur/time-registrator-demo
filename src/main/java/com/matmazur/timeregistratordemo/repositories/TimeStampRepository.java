01b28d431a9caa9f51173b605af39636930bd55b
Bingo Bobo <matmazur90@gmail.com>
2018-12-22 17:32:27 +0100

    added interface of repositorty

---
+++ src/main/java/com/matmazur/timeregistratordemo/repositories/TimeStampRepository.java
@@ -0,0 +1,6 @@
+package com.matmazur.timeregistratordemo.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TimeStampRepository extends CrudRepository {
+}
