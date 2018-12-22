dab50340f7e8d67dce454df4969561fec968a57c
Bingo Bobo <matmazur90@gmail.com>
2018-12-22 21:56:17 +0100

    added in main class setting timezone so hibernate wont save time -1h, new /add post method in main controller for adding times to the database, index.html holds form for TimeStamp creation

--- src/main/java/com/matmazur/timeregistratordemo/controller/IndexController.java
+++ src/main/java/com/matmazur/timeregistratordemo/controller/IndexController.java
@@ -1,12 +1,55 @@
 package com.matmazur.timeregistratordemo.controller;
 
+import com.matmazur.timeregistratordemo.model.TimeStamp;
+import com.matmazur.timeregistratordemo.repositories.TimeStampRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalTime;
+import java.util.TimeZone;
 
 @Controller
 public class IndexController {
 
-    public String index(){
+    private final
+    TimeStampRepository timeStampRepository;
+
+    @Autowired
+    public IndexController(TimeStampRepository timeStampRepository) {
+        this.timeStampRepository = timeStampRepository;
+    }
+
+
+    @GetMapping("/")
+    public String index(ModelMap modelMap) {
+
+        modelMap.put("formTimeStamp", new TimeStamp());
 
         return "index";
     }
+
+    @PostMapping("/add")
+    public String index(
+            @ModelAttribute TimeStamp formTimeStamp,
+            ModelMap modelMap,
+            @RequestParam int hour,
+            @RequestParam int minute,
+            @RequestParam int second
+    ) {
+
+        LocalTime time = LocalTime.of(hour, minute, second);
+
+        formTimeStamp.setTime(time);
+        timeStampRepository.save(formTimeStamp);
+        modelMap.put("formTimeStamp", formTimeStamp);
+
+        return "index";
+    }
+
+
 }
