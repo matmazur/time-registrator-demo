7c67a89b37c7fc876c71f2f5722ca967e3bc9be9
Bingo Bobo <matmazur90@gmail.com>
2018-12-22 16:55:32 +0100

    added packages for model repo and controller

--- src/main/java/com/matmazur/timeregistratordemo/TimeRegistratorDemoApplication.java
+++ src/main/java/com/matmazur/timeregistratordemo/TimeRegistratorDemoApplication.java
@@ -3,12 +3,14 @@ package com.matmazur.timeregistratordemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 @SpringBootApplication
 public class TimeRegistratorDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TimeRegistratorDemoApplication.class, args);
     }
-
 }
 
